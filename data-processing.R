cedadfd01f2bb265d0a113da90b7fcea74a40ed9
Tripp Bishop <dyne_erg@yahoo.com>
2025-09-20 20:04:31 -0600

    cleaned up the processing logic and removed data viz test.

--- data-processing.R
+++ data-processing.R
@@ -1,7 +1,8 @@
 library(tidyverse); library(janitor)
-df <- read_csv("tripp-data.csv") |> janitor::clean_names()
+df <- read_csv("most-of-the-data.csv") |> janitor::clean_names()
 
 
+# perform transformation and field engineering.
 df_processed <- df |> 
   pivot_longer(cols=c(everything(), -respondent), 
                names_to="category", values_to="response") |> 
@@ -24,12 +25,40 @@ df_processed <- df |>
                 .default=NA
     )
   ) |> 
-  select(respondent, question,brand,category,response) |> 
+  select(respondent,question,brand,category,response) |> 
   mutate(
     question = as_factor(question),
     brand = as_factor(brand)
   )
 
+
+# question one 
+df_processed |>
+  filter(question == 1) |> 
+  group_by(brand) |> 
+  summarise(
+    avg = mean(response, na.rm=TRUE),
+    stderr = sd(response),
+    min = min(response),
+    max = max(response)
+  )
+
+df_processed |>
+  filter(question == 1 & !is.na(response)) |> 
+  ggplot(aes(x=brand, y=response, colour=brand)) +
+  geom_boxplot() +
+  geom_jitter(alpha=0.5, height=0.1, width=0.4) +
+  scale_colour_brewer(palette="Dark2") +
+  labs(
+    y="Response",
+    x="Brand",
+    colour="Brand",
+    title="Comparison of brand rankings",
+    caption="Data points have been jittered both vertically and 
+             horizontally to prevent overplotting."
+  )
+
+# basic heat map disply for question 2
 df_processed |>
   filter(question == 2) |> 
   group_by(brand, category) |> 
@@ -38,7 +67,10 @@ df_processed |>
     stderr = sd(response),
     min = min(response),
     max = max(response)
-  )
+  ) |> ggplot(aes(brand, category, fill= avg)) + 
+  geom_tile(colour="white") +
+  scale_fill_viridis_b(direction=-1)
+
 
 df_processed |>
   filter(question == 2) |> 
