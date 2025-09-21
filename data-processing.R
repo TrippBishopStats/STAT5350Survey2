efbf4c2bb5571ff8faada295c7511d8f87d84ab1
Tripp Bishop <dyne_erg@yahoo.com>
2025-09-20 20:19:36 -0600

    updated to use complete data.

--- data-processing.R
+++ data-processing.R
@@ -1,5 +1,5 @@
 library(tidyverse); library(janitor)
-df <- read_csv("most-of-the-data.csv") |> janitor::clean_names()
+df <- read_csv("complete-data.csv") |> janitor::clean_names()
 
 
 # perform transformation and field engineering.
@@ -31,61 +31,4 @@ df_processed <- df |>
     brand = as_factor(brand)
   )
 
-
-# question one 
-df_processed |>
-  filter(question == 1) |> 
-  group_by(brand) |> 
-  summarise(
-    avg = mean(response, na.rm=TRUE),
-    stderr = sd(response),
-    min = min(response),
-    max = max(response)
-  )
-
-df_processed |>
-  filter(question == 1 & !is.na(response)) |> 
-  ggplot(aes(x=brand, y=response, colour=brand)) +
-  geom_boxplot() +
-  geom_jitter(alpha=0.5, height=0.1, width=0.4) +
-  scale_colour_brewer(palette="Dark2") +
-  labs(
-    y="Response",
-    x="Brand",
-    colour="Brand",
-    title="Comparison of brand rankings",
-    caption="Data points have been jittered both vertically and 
-             horizontally to prevent overplotting."
-  )
-
-# basic heat map disply for question 2
-df_processed |>
-  filter(question == 2) |> 
-  group_by(brand, category) |> 
-  summarise(
-    avg = mean(response),
-    stderr = sd(response),
-    min = min(response),
-    max = max(response)
-  ) |> ggplot(aes(brand, category, fill= avg)) + 
-  geom_tile(colour="white") +
-  scale_fill_viridis_b(direction=-1)
-
-
-df_processed |>
-  filter(question == 2) |> 
-  group_by(brand, category) |> 
-  summarise(
-    avg = mean(response),
-    stderr = sd(response),
-    min = min(response),
-    max = max(response)
-  )
-
-df_processed |>
-  filter(question == 2) |> 
-  group_by(brand, category, response) |> 
-  summarise(
-    count = n()
-  ) |> table()
-
+write_rds(df_processed, "df_proc.rds")
